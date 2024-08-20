10fe8f68394a4a77d4f4358be10356f61fdbff56
Craig O'Hagan <craig@justdevign.com.au>
2024-08-20 19:15:23 +1000

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -5,7 +5,7 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 describe("Token", () => {
-  let token, accounts, deployer;
+  let token, accounts, deployer, receiver;
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token");
@@ -13,6 +13,7 @@ describe("Token", () => {
 
     accounts = await ethers.getSigners();
     deployer = accounts[0];
+    receiver = accounts[1];
   });
 
   describe("Deployment", () => {
@@ -39,6 +40,52 @@ describe("Token", () => {
 
     it("Assigns total supply to deployer", async () => {
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
+  });
+
+  describe("Sending Tokens", () => {
+    let amount, transaction, result;
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .transfer(receiver.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("Transfer token balances", async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999900)
+        );
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+      });
+
+      it("Emits a Transfer event", async () => {
+        expect(await result.events[0].event).to.equal("Transfer");
+        expect(await result.events[0].args._from).to.equal(deployer.address);
+        expect(await result.events[0].args._to).to.equal(receiver.address);
+        expect(await result.events[0].args._value).to.equal(amount);
+      });
+    });
+
+    describe("Failure", () => {
+      it("Rejects insufficient balances", async () => {
+        const invalidAmount = tokens(100000000);
+        await expect(
+          token.connect(deployer).transfer(receiver.address, invalidAmount)
+        ).to.be.reverted;
+      });
+
+      it("Rejects invalid recipient", async () => {
+        const amount = tokens(100);
+        await expect(
+          token
+            .connect(deployer)
+            .transfer("0x0000000000000000000000000000000000000000", amount)
+        ).to.be.reverted;
+      });
     });
   });
 });
