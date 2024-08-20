c8dae46c1fa9b822983ee326a9bcceedf30e0a94
Craig O'Hagan <craig@justdevign.com.au>
2024-08-20 10:42:18 +1000

    3. Testing Contracts

---
+++ test/Token.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+describe("Token", () => {
+  let token;
+
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Hages", "HAGES", 1000000);
+  });
+
+  describe("Deployment", () => {
+    const name = "Hages";
+    const symbol = "HAGES";
+    const decimals = 18;
+    const totalSupply = tokens(1000000);
+
+    it("Has correct name", async () => {
+      expect(await token.name()).to.equal(name);
+    });
+
+    it("Has correct symbol", async () => {
+      expect(await token.symbol()).to.equal(symbol);
+    });
+
+    it("Has correct decimals", async () => {
+      expect(await token.decimals()).to.equal(decimals);
+    });
+
+    it("Has correct total supply", async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply);
+    });
+  });
+});
